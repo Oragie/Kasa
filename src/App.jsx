a7edcbfefb68906163ebd5cfc4ab012f9f5e4288
Vinko <vinkoloic@gmail.com>
2025-02-12 18:46:48 +0100

    add img, and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 import Hello from './components/Hello'
 
 function App() {
@@ -6,9 +9,11 @@ function App() {
 
   return (
     <>
+    <Header/>
     <Hello name={"Nous"}/>
     <Hello name={"Nous"}/>
     <Hello name={"Nous"}/>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,20 @@
+ 
+function Logo (){
+    return (
+        <>
+     {/* Lettre K */}
+   
+    <p className="logoK">K</p>
+
+     {/* Icône - Image */}
+    <img src="src\assets\img\logoIcon.png"
+    alt="Logo Icon"
+    className="logoIcon"/>
+
+    {/* Lettre sa */}
+    <p className="logoSa">sa</p>
+</>
+    );
+}
+
+export default Logo;
