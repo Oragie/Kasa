ce6b4bb1117a965ad911410ac2d21ee0d1a6281c
Vinko <vinkoloic@gmail.com>
2025-02-13 08:52:08 +0100

    add scss style, variable and global

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import './App.css'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 import Hello from './components/Hello'
+import { createBrowserRouter, NavLink, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <div>home</div>
+  },
+  {
+    path:"/about",
+    element: (
+    <div>À propos
+      <nav>
+        <NavLink to="/"/>
+        <NavLink to="/about">
+      </nav>
+    </div>
+    )
+  }
+]);
 
 function App() {
  
-
-  return (
-    <>
+  return (<>
+   <RouterProvider router={router}/>
+    
     <Header/>
     <Hello name={"Nous"}/>
     <Hello name={"Nous"}/>
