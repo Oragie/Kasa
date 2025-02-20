520557502ff5c2c52712079094a7b7797fec6a23
Vinko <vinkoloic@gmail.com>
2025-02-20 01:22:25 +0100

    feat: add Rating component with star rating display and tooltip

---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,26 @@
+import './rating.scss';
+import PropTypes from 'prop-types';
+import StarRating from '../Star';
+
+function Rating({ rating }) {
+    const stars = Array.from({ length: 5 }, (_, i) => parseFloat(Math.max(0, Math.min(1, rating - i)).toFixed(2)));
+
+    return (
+        <div className='allStarsNote' >
+            <div className='allStars'>
+                {stars.map((fill, index) => (
+
+                    <StarRating key={index} fill={fill} />
+
+                ))}
+            </div>
+            <p className='ratingBubule'>note {rating}/5</p>
+        </div>
+    );
+}
+
+Rating.propTypes = {
+    rating: PropTypes.string.isRequired,
+};
+
+export default Rating;
